d11df3ecb0b22f0e61d5d2b8d93ca14da5ffeeeb
finnovesh <finnovesh@gmail.com>
2022-02-17 22:33:47 +0530

    localfeed

--- lib/datafeed/localfeed.py
+++ lib/datafeed/localfeed.py
@@ -9,32 +9,47 @@ class localfeed():
       
       currentpointer=-1
       df=None
+      
 
       def __init__(cls,instrumentype,symbol, tf):
+          num_candles=5
           if instrumentype=="STOCK":
             interval = interval_to_number(tf) if interval_to_number(tf) > 1 else 1
 
             sourcedbsession, sourcedbengine = SourceSqlalchemy()
             with sourcedbengine.connect() as con:
-                df = pd.read_sql('SELECT TIMESTAMP as datetime, OPEN, HIGH, LOW, CLOSE, VOLUME, OI as openinterest FROM '+symbol+' LIMIT '+(200*interval).__str__(), con)
-                df['datetime']=pd.to_datetime(cls.df['datetime'])  
-                self.df = self.groupby(df,interval)         
+                df = pd.read_sql('SELECT TIMESTAMP as datetime, OPEN, HIGH, LOW, CLOSE, VOLUME, OI as openinterest FROM '+symbol+' LIMIT '+(num_candles*interval).__str__(), con)
+                df['datetime']=pd.to_datetime(df['datetime'])  
+                cls.df = df
+                cls.df = cls.groupby(interval)      
+
+                print(cls.df)   
                 cls.currentpointer=0               
                 con.close()
 
     
 
-      def groupby(self, df, interval):
+      def groupby(self, interval):
 
-        base = int(interval/2) if (interval % 2)==0 else 0
+        if interval==1 or interval==3 or interval==5 or interval==15:
+         base=0
+        elif interval==10:
+         base=5 
+        elif interval==30:
+         base=15
+        elif interval==60:
+         base=15
+        elif interval==1440:
+         base=555
 
-        df = df.set_index('datetime').resample(
+       
+        df = self.df.set_index('datetime').resample(
              interval.__str__()+'min', base=base).mean().dropna()
 
         return df
 
 if __name__ == '__main__':
-    obj = localfeed("STOCK","TCS", "15minute")
+    obj = localfeed("STOCK","TCS", "day")
     #print(obj.df)
    
    
